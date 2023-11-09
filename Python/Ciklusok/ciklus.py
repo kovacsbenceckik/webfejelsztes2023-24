001a18c79134e14016eb781b611fd954c5429c98
kovacsbenceckik <kovacs.bence@ckik.hu>
2023-11-09 10:22:04 +0100

    hello n

---
+++ Python/Ciklusok/ciklus.py
@@ -0,0 +1,4 @@
+n = int(input("hányszor fusson le? "))
+
+for i in range(n):
+    print(str(i+1) + ". Nigger")
---
+++ Python/Ciklusok/ciklus2.py
@@ -0,0 +1,7 @@
+n = int(input("hanyszor? "))
+
+i = 0
+
+while i < n:
+    i += 1 
+    print(f"{i}. Hello niga")
