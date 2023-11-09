4dcf85f8761c6012f69a4e75fcd46a25ab8e80d0
kovacsbenceckik <kovacs.bence@ckik.hu>
2023-11-09 10:35:43 +0100

    jegyek

---
+++ Python/Ciklusok/jegyek.py
@@ -0,0 +1,8 @@
+osztalyzat = int(input("osztalyzat: "))
+
+jo = 1 <= osztalyzat and osztalyzat <= 5
+while not jo:
+    osztalyzat = int(input("osztalyzat: "))
+
+jo = 1 <= osztalyzat and osztalyzat <= 5
+
