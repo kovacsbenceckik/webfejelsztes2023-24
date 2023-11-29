f8d1c687e722de3eee8d9f41897928b09b893093
kovacsbenceckik <kovacs.bence@ckik.hu>
2023-11-29 08:23:24 +0100

    tehetsegbol_kivettem_a_szokozoket

--- Python/kovacs.bence_tehetseg.py
+++ Python/kovacs.bence_tehetseg.py
@@ -8,10 +8,10 @@ for i in range(1,n+1):
      if i % 7 == 0:
            szam += i
 
-print(f"1-től {n}-ig a 7-tel oszthatók összege: ", szam)
+print(f"1-től {n}-ig a 7-tel oszthatók összege:", szam)
 
 while szam % oszto == 0:
     a += 1
     oszto *= 3
 
-print("Ennyiszer osztható 3-mal: ", a)
+print("Ennyiszer osztható 3-mal:", a)
