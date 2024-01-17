e18918f95441506552bf5a4b88464fed683449b5
kovacsbenceckik <kovacs.bence@ckik.hu>
2024-01-17 12:25:33 +0100

    lista input os cuc

---
+++ Python/Listák Beolvasása/elemszam.py
@@ -0,0 +1,6 @@
+# szamok = [2, 7, 4, 12, -3, 5]
+szamok = []
+for i in range(5):
+    bemenet = int(input("Nemzeti Adó- és Vámhivatal kér Öntől számadót:"))
+    szamok.append(bemenet)
+print(szamok)
