1aa5083dedeb2d71cf641efb8fa856a0bb03920f
kovacsbenceckik <kovacs.bence@ckik.hu>
2024-01-18 10:35:37 +0100

    mester1

--- Python/Listák Beolvasása/elemszam.py
+++ Python/Listák Beolvasása/elemszam.py
@@ -1,6 +1,18 @@
-# szamok = [2, 7, 4, 12, -3, 5]
+n = int(input("n: "))
 szamok = []
-for i in range(5):
+for i in range(n):
     bemenet = int(input("Nemzeti Adó- és Vámhivatal kér Öntől számadót:"))
     szamok.append(bemenet)
 print(szamok)
+
+#Összeg
+s = 0
+for i in range(n):
+    s += szamok[i]
+print("Számok összege:", s)
+
+#negyzetek
+negyzetek = []
+for i in range(n):
+    negyzetek.append(szamok[i]*szamok[i])
+print("Számok négyzetei:", negyzetek)
