d05e8a97fc9a30c0b96acb3f8e350d9858bca4f9
kovacsbenceckik <kovacs.bence@ckik.hu>
2024-02-09 09:38:28 +0100

    Cigányok random családszámban ♥

--- Python/Fuggvenyek/ido.py
+++ Python/Fuggvenyek/ido.py
@@ -1,4 +1,9 @@
-from time import time
+from time import time, sleep
 # print("ido:", time())
-print("random ", int(time()*100)%100)
+# print("random ", int(time() * 100) % 100)
 
+for i in range(40):
+    ido = time()
+    r = int(ido * 10000) % 10
+    print(r, end=" ")
+    sleep(0.001)
---
+++ Python/Fuggvenyek/veletlen.py
@@ -0,0 +1,8 @@
+from random import random, randrange
+# for i in range(400):
+#     r = int(random() * 6) + 1
+#     print("[0..6] random:", r)
+
+# [13..19]
+r = randrange(13, 19)
+print("[13..19] random:", r)
